443edd986446e10c04b26e1fea89c5e812bb2809
Piotr Modlinger <piotr.modlinger@gmail.com>
2017-12-11 13:02:54 +0100

    Gameplay app added

--- tictactoe/urls.py
+++ tictactoe/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from .views import welcome
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$',welcome)
 ]
