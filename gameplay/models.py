13ef1237c3fcb766f6a6e73c82fbf16b7d06fc14
Piotr Modlinger <piotr.modlinger@gmail.com>
2017-12-22 18:19:13 +0100

    View a board with moves

--- gameplay/models.py
+++ gameplay/models.py
@@ -11,6 +11,8 @@ GAME_STATUS_CHOICES = (
     ('D', 'Draw')
 )
 
+BOARD_SIZE = 3
+
 class GamesQuerySet(models.QuerySet):
     def games_for_user(self, user):
         return self.filter(
@@ -34,6 +36,15 @@ class Game(models.Model):
                               choices=GAME_STATUS_CHOICES)
     objects = GamesQuerySet.as_manager()
 
+    def board(self):
+        """Return a 2 dimentional list of Model objects,
+        so you can ask for the state of a square at position [y][x].
+        """
+        board = [[None for x in range(BOARD_SIZE)] for y in range(BOARD_SIZE)]
+        for move in self.move_set.all():
+            board[move.y][move.x] = move
+        return board
+
     def get_absolute_url(self):
         return reverse('gameplay_detail', args=[self.id])
 
@@ -45,8 +56,8 @@ class Move(models.Model):
     x = models.IntegerField()
     y = models.IntegerField()
     comment = models.CharField(max_length=300, blank=True)
-    by_first_player = models.BooleanField()
-    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    by_first_player = models.BooleanField(editable=False)
+    game = models.ForeignKey(Game, editable=False, on_delete=models.CASCADE)
 
     def __str__(self):
         return "{0} - {1}".format(self.game, self.comment)
