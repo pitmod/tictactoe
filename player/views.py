fc70ae8730fd62e7b7998ac99b8c2fd1c84ad318
Piotr Modlinger <piotr.modlinger@gmail.com>
2017-12-23 10:52:29 +0100

    Completed Pluralsight Course

--- player/views.py
+++ player/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 from .forms import InvitationForm
 from .models import Invitation
@@ -10,9 +13,11 @@ from gameplay.models import Game
 def home(request):
     my_games = Game.objects.games_for_user(request.user)
     active_games = my_games.active()
+    finished_games = my_games.difference(active_games)
     invitations = request.user.invitations_received.all()
     return render(request, "player/home.html",
-                  {'games': active_games,
+                  {'active_games': active_games,
+                   'finished_games': finished_games,
                    'invitations': invitations})
 
 @login_required()
@@ -45,3 +50,9 @@ def accept_invitation(request, id):
                       "player/accept_invitation_form.html",
                       {'invitation': invitation}
                       )
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = "player/signup_form.html"
+    success_url = reverse_lazy('player_home')
